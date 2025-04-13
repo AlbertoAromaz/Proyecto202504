bb436b48c7dc8708b856c63577a493d74df0f078
Alberto Zamora <albertosc.lima@gmail.com>
2025-04-13 14:03:02 +0200

    Actualizar

--- app/charts.py
+++ app/charts.py
@@ -3,14 +3,16 @@ import matplotlib.pyplot as plt
 def generate_bar_chart(name, labels, values):
   fig, ax = plt.subplots()
   ax.bar(labels, values)
+  #ptl.show()
   plt.savefig(f'./imgs/{name}_bar.png')
   plt.close()
 
-def generate_pie_chart(name, labels, values):
+def generate_pie_chart(labels, values):
   fig, ax = plt.subplots()
   ax.pie(values, labels=labels)
   ax.axis('equal')
-  plt.savefig(f'./imgs/{name}_pie.png')
+  #ptl.show()
+  plt.savefig('pie.png')
   plt.close()
 
 if __name__ == '__main__':
--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 import utils
 import read_csv
 import charts
-#import pandas as pd
 
 def run():
   df = read_csv.read_csv('data.csv')
@@ -12,16 +11,15 @@ def run():
   charts.generate_pie_chart(countries,percentages)
 
   country = input('Type Country => ')
-  print(country)
+  #print(country)
   
   result = utils.population_by_country(df, country)
   
   if len(result) > 0:
     country = result[0]
-    print(country)
+    #print(country)
     labels, values = utils.get_population(country)
-    country = input('Type Country => ')
-    charts.generate_bar_chart(country, labels, values)
+    charts.generate_bar_chart(country['Country'], labels, values)
  
 if __name__ == '__main__':
   run()
